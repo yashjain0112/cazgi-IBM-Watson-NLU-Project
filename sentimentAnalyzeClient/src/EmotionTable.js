eeb5b7e859d820f35573668f6bff77b43a43cec2
Yash Jain <56643041+yashjain0112@users.noreply.github.com>
2021-06-27 13:41:24 +0530

    Update EmotionTable.js

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -1,28 +1,22 @@
-
 import React from 'react';
 import './bootstrap.min.css';
 
 class EmotionTable extends React.Component {
     render() {
-        const emotionstomap = this.props.emotions
-        const mappingemotions = Object.keys(emotionstomap).map(ems =>
-            {
-                console.log(ems)
-                return(
-                    <>
-                    <tr>
-                    <td>{ems}</td>
-                    <td>{emotionstomap[ems]}</td>
-                    </tr>
-                    </>
-                )
-            })
       return (  
         <div>
-
           <table className="table table-bordered">
             <tbody>
-            {mappingemotions}
+            {
+                Object.entries(this.props.emotions).map(function(mapentry) {
+                return (
+                   <tr>
+                   <td>{mapentry[0]}</td>
+                   <td>{mapentry[1]}</td>
+                   </tr>
+                  )
+            })
+            }
             </tbody>
           </table>
           </div>
